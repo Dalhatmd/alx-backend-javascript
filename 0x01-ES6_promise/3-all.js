8eeadf7999fd4586bbd1e281ed5146e75aebf940
dalhatmd <umarwalid4055@gmail.com>
2024-10-02 17:02:54 +0000

    Add task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import {createUser, uploadPhoto} from './utils.js'
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+        console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+  .catch((error) => {
+    console.log("Signup system offline");
+  });
+}
