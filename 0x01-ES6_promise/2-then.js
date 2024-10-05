0ec2e0cd5d6b57ab7a491fe2a90fef48965c4006
dalhatmd <umarwalid4055@gmail.com>
2024-10-05 19:17:22 +0000

    Fix Eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((result) => {
+    .then(() => {
       return {"status": 200, "body": "success"};
     })
     .catch((error) => {
--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,5 @@
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName,
+    firstName, lastName
   });
 }
