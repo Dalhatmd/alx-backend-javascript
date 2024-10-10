70ab46a31b6d1ed7f702b899b8d932948edc1572
dalhatmd <umarwalid4055@gmail.com>
2024-10-10 14:13:17 +0000

    Add task 1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher = {
+  readonly firstName: string;
+  fullTimeEmployee: boolean;
+  readonlylastName: string;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
