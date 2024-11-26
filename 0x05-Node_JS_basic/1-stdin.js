53f9f037277a915dd68984d26150cd52f4d935aa
Dalhatmd <umarwalid4055@gmail.com>
2024-11-26 15:38:00 +0100

    Add task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+let userInput = '';
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (input) => {
+  userInput = input.trim();
+  console.log(`Your name is: ${userInput}`);
+});
+
+process.on('exit', (code) => {
+  console.log('This important software is now closing');
+});
+
+console.log("Welcome to Holberton School, what is your name?\n");
