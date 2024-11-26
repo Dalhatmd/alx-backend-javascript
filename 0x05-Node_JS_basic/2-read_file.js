523ccedc0cd0b23aa5fb4b350dfd925859660213
Dalhatmd <umarwalid4055@gmail.com>
2024-11-26 17:14:51 +0100

    Fix task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,41 +1,34 @@
 const fs = require('fs');
 
-// Function to count the number of students and the occurrences of each field
-function countStudents(filePath) {
-  const fieldCount = {};
+function countStudents(path) {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
 
-  try {
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
-    const lines = fileContent.split('\n');
-
-    // Log the number of students (lines minus the header)
-    console.log('Number of students:', lines.length - 1);
-    // Iterate over the lines (skip the first line)
-    lines.slice(1).forEach((line) => {
-      const values = line.split(',');
-      const firstName = values[0].split(',');
-      const field = values[3].trim();
-      
-      // Count the occurrences of each field
-      if (!fieldCount[field]) {
-        fieldCount[field] = {
-            count: 0,
-            firstNames: []
-        };
+  const in_file = fs.readFileSync(path, 'utf8');
+  const lines = in_file.split('\n');
+  const fieldName = {};
+  let students = -1;
+  for (const line of lines) {
+    if (line.trim() !== '') {
+      const columns = line.split(',');
+      const field = columns[3];
+      const firstname = columns[0];
+      if (students >= 0) {
+        if (!Object.hasOwnProperty.call (fieldName, field)) {
+         fieldName[field] = [];
+        }
+     fieldName[field] = [... fieldName[field], firstname];
       }
-      fieldCount[field].count++;
-      fieldCount[field].firstNames.push(firstName);
-    });
-
-    Object.keys(fieldCount).forEach((field) => {
-        console.log(`Number of students in ${field}: ${fieldCount[field].count}. List: ${fieldCount[field].firstNames.join(', ')}`);
-    });
-
-
-  } catch (err) {
-    console.log('Cannot load the database');
+      students += 1;
+    }
+  }
+  console.log(`Number of students: ${students}`);
+  for (const key in fieldName) {
+    if (Object.hasOwnProperty.call (fieldName, key)) {
+      console.log(`Number of students in ${key}: ${fieldName[key].length}. List: ${fieldName[key].join(', ')}`);
+    }
   }
 }
-
 
 module.exports = countStudents;
