7efdd63f0a7303786951335f4322806826e94db2
Dalhatmd <umarwalid4055@gmail.com>
2024-11-28 22:58:09 +0100

    fixes

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,7 +1,7 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 const Utils = require('./utils');
-const { sendPaymentRequestToApi } = require('./payment');
+const { sendPaymentRequestToApi } = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
     let calculateNumberStub;
