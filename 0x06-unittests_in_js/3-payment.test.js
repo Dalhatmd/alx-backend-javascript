bda6e1a2e4971b14a12bcdec7875e29b7b7ec174
Dalhatmd <umarwalid4055@gmail.com>
2024-11-28 22:40:23 +0100

    Add task 3

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const { sendPaymentRequestToApi } = require('./payment');
+
+describe('sendPaymentRequestToApi', () => {
+    let calculateNumberStub;
+
+    beforeEach(() => {
+        // Create a stub for Utils.calculateNumber
+        calculateNumberStub = sinon.stub(Utils.prototype, 'calculateNumber');
+        calculateNumberStub.withArgs('SUM', 100, 20).returns(120);
+    });
+
+    afterEach(() => {
+        // Restore the stub
+        calculateNumberStub.restore();
+    });
+
+    it('should use Utils.calculateNumber', () => {
+        const result = sendPaymentRequestToApi(100, 20);
+
+        // Verify the stub was called with the correct arguments
+        sinon.assert.calledWith(calculateNumberStub, 'SUM', 100, 20);
+        
+        // Verify the result
+        expect(result).to.equal(120);
+    });
+});
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,23 @@
+class Utils {
+  calculateNumber(type, a, b) {
+    a = Math.round(a);
+    b = Math.round(b);
+  
+    switch (type) {
+      case "SUM":
+        return a + b;
+      
+      case "SUBTRACT":
+        return a - b;
+
+      case "DIVIDE":
+        if (b === 0) {
+          return 'Error';
+        } else {
+          return a / b;
+        }
+    }
+  };
+}
+
+module.exports = Utils;
