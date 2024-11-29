1c507c987c81c02c16646dc38d06a77f722dc4c3
Dalhatmd <umarwalid4055@gmail.com>
2024-11-29 17:27:40 +0100

    fix task 3 and add task 5

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,9 +2,9 @@ const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is ${result}`);
+  console.log(`The total is: ${result}`);
   return result;
 }
 
 
-module.exports = { sendPaymentRequestToApi };
+module.exports = sendPaymentRequestToApi;
---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,24 @@
+const sinon = require('sinon');
+const { sendPaymentRequestToApi } = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(function () {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(function () {
+    consoleSpy.restore();
+});
+
+  it('should log "The Total is: 120"', function () {
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnceWithExactly(consoleSpy, 'The total is: 120');
+  });
+
+  it('should log "The Total is: 20"', function () {
+    sendPaymentRequestToApi(10, 10);
+    sinon.assert.calledOnceWithExactly(consoleSpy, 'The total is: 20');
+  });
+});
